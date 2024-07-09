dc831d412c20dad5d89e2e98739c93db36e8eb03
JackFed <jack.fedurek@gmail.com>
2024-07-09 11:59:32 -0400

    Breaking down spotify api into needed elements

--- src/components/StoreItems.jsx
+++ src/components/StoreItems.jsx
@@ -4,7 +4,9 @@ import { useState } from "react";
 export default function StoreItems() {
     
     const [inventory, setInventory] = useState(["Item1", "Item2"]); // Potentially useState to save api called items to local storage
-
+    // Albums: { name: AlbumName, release: Release date (Specify day/year), id, imageUrl, artists}
+    // artists: { id, name, externalUrl(spotify page)}
+    // call get album from album id to get all tracks and info
 
     // TODO: Create function calls to fetch images and values for list items
     // Add UUI to each inventory item to use for React's key property
