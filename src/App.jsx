3369ce7c72ca2b732921b9727d431f430ce68d38
JackFed <jack.fedurek@gmail.com>
2024-07-12 16:36:00 -0400

    Add nav bar outline

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import StoreItems from "./components/StoreItems"
+import HomePage from "./components/HomePage"
+import NavBar from "./components/NavBar"
 
 function App() {
 
   return (
     <>
+      <NavBar />
+      <HomePage />
       <StoreItems />
     </>
   )
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+
+    return (
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Store</a></li>
+                <li><a href="#">My Cart</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
