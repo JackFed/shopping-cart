d2b687e90234ebd18e85216f69f2c220f9937661
JackFed <jack.fedurek@gmail.com>
2024-07-15 11:06:06 -0400

    Create Album jsx page to display info and add to cart view

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import StoreItems from "./components/StoreItems"
 import HomePage from "./components/HomePage"
 import NavBar from "./components/NavBar"
+import Album from "./components/Album"
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <NavBar />
       <HomePage />
       <StoreItems />
+      <Album />
     </>
   )
 }
---
+++ src/components/Album.jsx
@@ -0,0 +1,34 @@
+const Album = () => {
+
+    return (
+        <div className="album">
+            <img src="#" alt="Album image" />
+            <div className="info">
+                <h1>Album Title</h1>
+                <h2>Artist(s)</h2>
+                <p>Label: Label name</p>
+                <p>Release date: July 20, 1929</p>
+            </div>
+            <div className="tracks">
+                <h1>Track List</h1>
+                <ul className="track-list">
+                    <li>Track 1</li>
+                    <li>Track 2</li>
+                    <li>Track 3</li>
+                </ul>
+            </div>
+            <div className="order">
+                <div className="pricing">$29.99</div>
+                <div className="amount">
+                    <p>Quantity</p>
+                    <button>-</button>
+                    <div className="quantity">1</div>                    
+                    <button>+</button>
+                </div>
+                <div className="add-cart">Add To Cart</div>
+            </div>
+        </div>
+    )
+}
+
+export default Album;
